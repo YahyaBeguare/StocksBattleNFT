4feb9f2bd389d090cb209c187ffeee12f0c4df5b
unknown <beguareyahya01@gmail.com>
2025-03-20 23:57:15 +0000

    feat: front end implementation

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Cross Chain NFTs of Stocks Volatility </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
